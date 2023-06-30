21b414b25bd3988975ab32963be64ffbd3d2a5fc
Derrick Hammer <derrick@derrickhammer.com>
2023-06-30 02:18:35 -0400

    fix: fix plugins list

--- src/index.ts
+++ src/index.ts
@@ -22,10 +22,12 @@ export default async function (context: PresetContext): Promise<PresetAsset> {
     supplementaryConfig: {
       rollup: {
         plugins: [
-          "@apply @rollup/plugin-wasm[default]",
-          {
-            targetEnv: "auto-inline",
-          },
+          [
+            "@apply @rollup/plugin-wasm[default]",
+            {
+              targetEnv: "auto-inline",
+            },
+          ],
         ],
       },
     },
