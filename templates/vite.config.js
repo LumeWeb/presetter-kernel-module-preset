36d799d80120fd47ffeb6f638bc04b5472f90875
Derrick Hammer <derrick@derrickhammer.com>
2023-07-08 03:32:49 -0400

    refactor: Revert "feat: add wasm plugin"
    This reverts commit 5a72c3c946e6ab1a53a0d1f7c3681dc3d8054de3.

--- templates/vite.config.js
+++ templates/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import optimizer from "vite-plugin-optimizer";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import wasm from "vite-plugin-wasm";
 export default defineConfig({
   build: {viteBuild},
   resolve: {viteResolve},
   plugins: [
-    wasm(),
     optimizer({viteOptimize}),
     nodePolyfills({vitePolyfill}),
   ],
